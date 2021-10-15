b997dc790c06142e80acb63175cbc1bb73ae153f
Deborah Dike <deborah.r.dike@gmail.com>
2021-10-15 11:09:41 +0000

    Add script for Tetris block

---
+++ assets/js/block.js
@@ -0,0 +1,47 @@
+// Tetris blocks represented in 3x3 matrixes
+const BLOCK_TYPES = {
+    L: [[1, 0, 0],
+        [1, 0, 0],
+        [1, 1, 0]],
+    J: [[0, 1, 1],
+        [0, 0, 1],
+        [0, 0, 1]],
+    Z: [[1, 1, 0],
+        [0, 1, 0]
+        [0, 1, 1]],
+    S: [[0, 1, 1,
+        [0, 1, 0],
+        [1, 1, 0]]],
+    SQ: [[0, 0, 0],
+        [0, 1, 1],
+        [0, 1, 1]],
+    I: [[1, 0, 0],
+        [1, 0, 0]
+        [1, 0, 0]],
+    T: [[1, 1, 1],
+        [0, 1, 0],
+        [0, 0, 0]]
+};
+const BLOCK_SIZE = 20; // Multiplier for calculating board size
+
+const BLOCKS = [BLOCK_TYPES.L, BLOCK_TYPES.J,
+                BLOCK_TYPES.Z, BLOCK_TYPES.S,
+                BLOCK_TYPES.SQ,
+                BLOCK_TYPES.I, BLOCK_TYPES.T];
+
+// Stores the tetris block represented by the Block object
+let currentBlock = [];
+
+class Block {
+    constructor() {
+        currentBlock = BLOCKS[this.getRandNumber()];
+    }
+
+    /**
+     * 
+     * @returns a random number between 0 and 6;
+     */
+    getRandNumber(){
+        return Math.floor(Math.random() * 7);
+    }
+}
